656469e7f8b59f63aedfa66fce97672f35f2734e
RajeevPrakashAD1 <prakashrajeev115@gmail.com>
2021-11-05 13:19:27 +0530

    dfjka

--- socket/socketServer.js
+++ socket/socketServer.js
@@ -181,7 +181,7 @@ setInterval(() => {
 	console.log('userInfo= ', userInfo);
 	console.log('users = ', users);
 	console.log('.............................');
-}, 60 * 1000);
+}, 3*60 * 1000);
 // module.exports = server;
 
 server.listen(8000, () => {
